a11fd808367e55da517806f9f66f387fd5c921a2
BstForge <157843230+BstForge@users.noreply.github.com>
2025-08-21 17:18:01 -0500

    0.0.03 – Layout Shell Wired

---
+++ menus/top/TopMenu.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TheWordForge.menus.top;
+
+public partial class TopMenu : UserControl
+{
+    public TopMenu()
+    {
+        InitializeComponent();
+    }
+}
